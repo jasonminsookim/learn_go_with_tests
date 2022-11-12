381c112162b10d1cebeb138f8dc2cd8b21413fbe
Jason Kim <jasonminsookim@gmail.com>
2022-11-12 23:08:44 +0000

    use go work

--- 2_integers/adder_test.go
+++ 2_integers/adder_test.go
@@ -6,8 +6,8 @@ import (
 )
 
 func TestAdder(t *testing.T) {
-	sum := Add(2, 2)
-	expected := 4
+	sum := Add(-1, 3)
+	expected := 2
 
 	if sum != expected {
 		t.Errorf("expected '%d' but got '%d'", expected, sum)
